ab96bf16f8b746e758432d1628a3f8fcb3a7684a
Uladzislau Zamkavets <zamkavets.public@gmail.com>
2023-05-24 15:30:47 +0200

    added state inside of the class

--- src/SpecialClassForTextareaAutosize.js
+++ src/SpecialClassForTextareaAutosize.js
@@ -4,6 +4,9 @@ import TextareaAutosize from 'react-textarea-autosize';
 class SpecialClassForTextareaAutosize extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            questionText:"123"
+        }
     }
 
     render() {
@@ -13,8 +16,8 @@ class SpecialClassForTextareaAutosize extends React.Component {
                     style={{ width: "100%", border: "none", outline: "none", resize: "none", backgroundColor: "transparent", overflow: "hidden", fontStyle: "italic", fontSize: "1em", fontFamily: "Arial" }}
                     id="untypedAnswer"
                     name="untypedAnswer"
-                    value={this.props.taValue}
-                    onChange={(event) => this.props.handleChange(event)}
+                    value={this.state.questionText}
+                    onChange={(event) => this.setState({ questionText: event.target.value })}
                     placeholder="Type the untyped answer."
                 />
             </div>
